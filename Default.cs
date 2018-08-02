2de4567734e2ad7e2e8da0c835fd33184c96ddf2
storm37000 <darkrai37@hotmail.com>
2018-08-01 21:03:42 -0400

    initial commit

---
+++ Default.cs
@@ -0,0 +1,63 @@
+﻿using Smod2;
+using Smod2.Attributes;
+using Smod2.Events;
+using Smod2.EventHandlers;
+
+namespace Smod.TestPlugin
+{
+    [PluginDetails(
+        author = "storm37000",
+        name = "ServerNameVars",
+        description = "Readds server name statistics plus some new ones",
+        id = "s37k.servernamevars",
+        version = "1.0.0",
+        SmodMajor = 3,
+        SmodMinor = 1,
+        SmodRevision = 0
+        )]
+    class Default : Plugin
+    {
+        public override void OnDisable()
+        {
+            this.Info(this.Details.name + " has been disabled.");
+        }
+        public override void OnEnable()
+        {
+            bool SSLerr = false;
+            this.Info(this.Details.name + " has been enabled.");
+            while (true)
+            {
+                try
+                {
+                    string host = "https://storm37000.tk/addons/";
+                    if (SSLerr) { host = "http://74.91.115.126/addons/"; }
+                    ushort version = ushort.Parse(this.Details.version.Replace(".", string.Empty));
+                    ushort fileContentV = ushort.Parse(new System.Net.WebClient().DownloadString(host + this.Details.name + ".ver"));
+                    if (fileContentV > version)
+                    {
+                        this.Info("Your version is out of date, please visit the Smod discord and download the newest version");
+                    }
+                    break;
+                }
+                catch (System.Exception e)
+                {
+                    if (SSLerr == false && e.Message.Contains("The authentication or decryption has failed."))
+                    {
+                        SSLerr = true;
+                        continue;
+                    }
+                    this.Error("Could not fetch latest version txt: " + e.Message);
+                    break;
+                }
+            }
+        }
+
+        public override void Register()
+        {
+            // Register Events
+            EventHandler events = new EventHandler(this);
+            this.AddEventHandler(typeof(IEventHandlerSetServerName), events, Priority.Highest);
+            this.AddEventHandler(typeof(IEventHandlerRoundStart), events, Priority.Highest);
+        }
+    }
+}
---
+++ EventHandler.cs
@@ -0,0 +1,83 @@
+﻿using Smod2;
+using Smod2.Events;
+using Smod2.EventHandlers;
+
+namespace Smod.TestPlugin
+{
+    class EventHandler : IEventHandlerSetServerName, IEventHandlerRoundStart
+    {
+        private Plugin plugin;
+        public uint RoundNumber { get; private set; } = 0;
+
+        public EventHandler(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        private string fullPlayerCount(SetServerNameEvent ev)
+        {
+            if(ev.Server.NumPlayers/ev.Server.MaxPlayers == 1)
+            {
+                return "FULL";
+            }
+            else
+            {
+                return ev.Server.NumPlayers + "/" + ev.Server.MaxPlayers;
+            }
+        }
+
+        private string warheadDetonated(SetServerNameEvent ev)
+        {
+            if (ev.Server.Round.Stats.WarheadDetonated)
+            {
+                return "☢ WARHEAD DETONATED ☢";
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public void OnSetServerName(SetServerNameEvent ev)
+        {
+            string cfgname = ConfigManager.Manager.Config.GetStringValue("sm_server_name", ev.ServerName);
+
+            cfgname = cfgname.Replace("$player_count", "" + ev.Server.NumPlayers);
+            cfgname = cfgname.Replace("$max_players", "" + ev.Server.MaxPlayers);
+            cfgname = cfgname.Replace("$full_player_count", fullPlayerCount(ev));
+            cfgname = cfgname.Replace("$port", "" + ev.Server.Port);
+            cfgname = cfgname.Replace("$ip", ev.Server.IpAddress);
+            //cfgname = cfgname.Replace("$number", "" + (ev.Server.Port - ConfigFile.GetIntList("port_queue")[0] + 1));
+            //cfgname = cfgname.Replace("$lobby_id", "-");
+            //cfgname = cfgname.Replace("$version", "-");
+            //cfgname = cfgname.Replace("$sm_version", smodtrackstr);
+            cfgname = cfgname.Replace("$classd_alive", "" + ev.Server.Round.Stats.ClassDAlive);
+            cfgname = cfgname.Replace("$classd_escape", "" + ev.Server.Round.Stats.ClassDEscaped);
+            cfgname = cfgname.Replace("$classd_start", "" + ev.Server.Round.Stats.ClassDStart);
+            cfgname = cfgname.Replace("$classd_dead", "" + ev.Server.Round.Stats.ClassDDead);
+            //cfgname = cfgname.Replace("$classd_counter", "" + ev.Server.NumPlayers);
+            cfgname = cfgname.Replace("$scientists_alive", "" + ev.Server.Round.Stats.ScientistsAlive);
+            cfgname = cfgname.Replace("$scientists_escape", "" + ev.Server.Round.Stats.ScientistsEscaped);
+            cfgname = cfgname.Replace("$scientists_start", "" + ev.Server.Round.Stats.ScientistsStart);
+            cfgname = cfgname.Replace("$scientists_dead", "" + ev.Server.Round.Stats.ScientistsDead);
+            //cfgname = cfgname.Replace("$scientists_counter", "" + ev.Server.NumPlayers);
+            cfgname = cfgname.Replace("$scp_alive_z", "" + ev.Server.Round.Stats.SCPAlive);
+            cfgname = cfgname.Replace("$scp_alive_noz", "" + (ev.Server.Round.Stats.SCPAlive - ev.Server.Round.Stats.Zombies));
+            cfgname = cfgname.Replace("$scp_start", "" + ev.Server.Round.Stats.SCPStart);
+            cfgname = cfgname.Replace("$scp_dead", "" + ev.Server.Round.Stats.SCPDead);
+            cfgname = cfgname.Replace("$scp_zombies", "" + ev.Server.Round.Stats.Zombies);
+            cfgname = cfgname.Replace("$scp_kills", "" + ev.Server.Round.Stats.SCPKills);
+            //cfgname = cfgname.Replace("$scp_counter", "" + ev.Server.Round.Stats);
+            cfgname = cfgname.Replace("$warhead_detonated", warheadDetonated(ev));
+            cfgname = cfgname.Replace("$round_duration", "" + ev.Server.Round.Duration/60);
+            cfgname = cfgname.Replace("$round_number", "" + RoundNumber);
+
+            ev.ServerName = cfgname;
+        }
+
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+            RoundNumber++;
+        }
+    }
+}
