389b0472bd2da75e33259e4c6b54a867cf1a64f0
storm37000 <6331913+storm37000@users.noreply.github.com>
2019-01-02 16:40:33 -0500

    use tabs instead of spaces

--- EventHandler.cs
+++ EventHandler.cs
@@ -5,43 +5,43 @@ using System;
 
 namespace ServerNameVars
 {
-    class EventHandler : IEventHandlerSetServerName, IEventHandlerRoundStart, IEventHandlerRoundEnd, IEventHandlerPlayerDie, IEventHandlerCheckEscape, IEventHandlerSetRole, IEventHandlerWarheadDetonate
-    {
-        private Main plugin;
-        private System.Collections.Generic.List<string> blklst = new System.Collections.Generic.List<string>();
-        private System.Collections.Generic.Dictionary<string, Func<string>> cmdtable = new System.Collections.Generic.Dictionary<string, Func<string>>();
+	class EventHandler : IEventHandlerSetServerName, IEventHandlerRoundStart, IEventHandlerRoundEnd, IEventHandlerPlayerDie, IEventHandlerCheckEscape, IEventHandlerSetRole, IEventHandlerWarheadDetonate
+	{
+		private Main plugin;
+		private System.Collections.Generic.List<string> blklst = new System.Collections.Generic.List<string>();
+		private System.Collections.Generic.Dictionary<string, Func<string>> cmdtable = new System.Collections.Generic.Dictionary<string, Func<string>>();
 
-        public ushort RoundNumber { get; private set; } = 0;
-        public ushort SCPKills { get; private set; } = 0;
-        public ushort ClassDEscapes { get; private set; } = 0;
-        public ushort ScientistEscapes { get; private set; } = 0;
-        public ushort ClassDStart { get; private set; } = 0;
-        public ushort ScientistStart { get; private set; } = 0;
-        public ushort SCPStart { get; private set; } = 0;
-        public bool WarheadDetonated { get; private set; } = false;
+		public ushort RoundNumber { get; private set; } = 0;
+		public ushort SCPKills { get; private set; } = 0;
+		public ushort ClassDEscapes { get; private set; } = 0;
+		public ushort ScientistEscapes { get; private set; } = 0;
+		public ushort ClassDStart { get; private set; } = 0;
+		public ushort ScientistStart { get; private set; } = 0;
+		public ushort SCPStart { get; private set; } = 0;
+		public bool WarheadDetonated { get; private set; } = false;
 
-        public EventHandler(Main plugin)
-        {
-            this.plugin = plugin;
-        }
+		public EventHandler(Main plugin)
+		{
+			this.plugin = plugin;
+		}
 
-        public void addCustomVar(string varname, Func<string> callback, Plugin source)
-        {
-            cmdtable[varname] = callback;
-            plugin.Info("Added custom var: $[" + varname + "]" + " From plugin: " + source.Details.id);
-        }
+		public void addCustomVar(string varname, Func<string> callback, Plugin source)
+		{
+			cmdtable[varname] = callback;
+			plugin.Info("Added custom var: $[" + varname + "]" + " From plugin: " + source.Details.id);
+		}
 
-        private void reset()
-        {
-            blklst.Clear();
-            SCPKills = 0;
-            ClassDEscapes = 0;
-            ScientistEscapes = 0;
-            ClassDStart = 0;
-            ScientistStart = 0;
-            SCPStart = 0;
-            WarheadDetonated = false;
-        }
+		private void reset()
+		{
+			blklst.Clear();
+			SCPKills = 0;
+			ClassDEscapes = 0;
+			ScientistEscapes = 0;
+			ClassDStart = 0;
+			ScientistStart = 0;
+			SCPStart = 0;
+			WarheadDetonated = false;
+		}
 
 //        private string Counter(SetServerNameEvent ev)
 //        {
@@ -55,129 +55,129 @@ namespace ServerNameVars
 //            }
 //        }
 
-        private string Counter(ushort current, ushort max)
-        {
-            return current + "/" + max;
-        }
+		private string Counter(ushort current, ushort max)
+		{
+			return current + "/" + max;
+		}
 
-        public void OnSetServerName(SetServerNameEvent ev)
-        {
-            string cfgname = ConfigManager.Manager.Config.GetStringValue("sm_server_name", ev.ServerName);
+		public void OnSetServerName(SetServerNameEvent ev)
+		{
+			string cfgname = ConfigManager.Manager.Config.GetStringValue("sm_server_name", ev.ServerName);
 
-            //cfgname = cfgname.Replace("$player_count", "" + ev.Server.NumPlayers);
-            //cfgname = cfgname.Replace("$max_players", "" + ev.Server.MaxPlayers);
-            //cfgname = cfgname.Replace("$full_player_count", Counter(ev));
-            //cfgname = cfgname.Replace("$port", "" + ev.Server.Port);
-            //cfgname = cfgname.Replace("$ip", ev.Server.IpAddress);
-            //cfgname = cfgname.Replace("$number", "" + (ev.Server.Port - ConfigFile.GetIntList("port_queue")[0] + 1));
-            //cfgname = cfgname.Replace("$lobby_id", "-");
-            //cfgname = cfgname.Replace("$version", "-");
-            //cfgname = cfgname.Replace("$sm_version", smodtrackstr);
-            cfgname = cfgname.Replace("$classd_alive", "" + ev.Server.Round.Stats.ClassDAlive);
-            cfgname = cfgname.Replace("$classd_escape", "" + ClassDEscapes);
-            cfgname = cfgname.Replace("$classd_start", "" + ClassDStart);
-            cfgname = cfgname.Replace("$classd_dead", "" + ev.Server.Round.Stats.ClassDDead);
-            cfgname = cfgname.Replace("$classd_counter", "" + Counter(ClassDEscapes, ClassDStart));
-            cfgname = cfgname.Replace("$scientists_alive", "" + ev.Server.Round.Stats.ScientistsAlive);
-            cfgname = cfgname.Replace("$scientists_escape", "" + ScientistEscapes);
-            cfgname = cfgname.Replace("$scientists_start", "" + ScientistStart);
-            cfgname = cfgname.Replace("$scientists_dead", "" + ev.Server.Round.Stats.ScientistsDead);
-            cfgname = cfgname.Replace("$scientists_counter", "" + Counter(ScientistEscapes, ScientistStart));
-            cfgname = cfgname.Replace("$scp_alive", "" + ev.Server.Round.Stats.SCPAlive);
-            cfgname = cfgname.Replace("$scp_start", "" + SCPStart);
-            cfgname = cfgname.Replace("$scp_dead", "" + ev.Server.Round.Stats.SCPDead);
-            cfgname = cfgname.Replace("$scp_zombies", "" + ev.Server.Round.Stats.Zombies);
-            cfgname = cfgname.Replace("$scp_kills", "" + SCPKills);
-            cfgname = cfgname.Replace("$scp_counter", "" + Counter((ushort)ev.Server.Round.Stats.SCPAlive, SCPStart));
-            cfgname = cfgname.Replace("$ntf_alive", "" + ev.Server.Round.Stats.NTFAlive);
+			//cfgname = cfgname.Replace("$player_count", "" + ev.Server.NumPlayers);
+			//cfgname = cfgname.Replace("$max_players", "" + ev.Server.MaxPlayers);
+			//cfgname = cfgname.Replace("$full_player_count", Counter(ev));
+			//cfgname = cfgname.Replace("$port", "" + ev.Server.Port);
+			//cfgname = cfgname.Replace("$ip", ev.Server.IpAddress);
+			//cfgname = cfgname.Replace("$number", "" + (ev.Server.Port - ConfigFile.GetIntList("port_queue")[0] + 1));
+			//cfgname = cfgname.Replace("$lobby_id", "-");
+			//cfgname = cfgname.Replace("$version", "-");
+			//cfgname = cfgname.Replace("$sm_version", smodtrackstr);
+			cfgname = cfgname.Replace("$classd_alive", "" + ev.Server.Round.Stats.ClassDAlive);
+			cfgname = cfgname.Replace("$classd_escape", "" + ClassDEscapes);
+			cfgname = cfgname.Replace("$classd_start", "" + ClassDStart);
+			cfgname = cfgname.Replace("$classd_dead", "" + ev.Server.Round.Stats.ClassDDead);
+			cfgname = cfgname.Replace("$classd_counter", "" + Counter(ClassDEscapes, ClassDStart));
+			cfgname = cfgname.Replace("$scientists_alive", "" + ev.Server.Round.Stats.ScientistsAlive);
+			cfgname = cfgname.Replace("$scientists_escape", "" + ScientistEscapes);
+			cfgname = cfgname.Replace("$scientists_start", "" + ScientistStart);
+			cfgname = cfgname.Replace("$scientists_dead", "" + ev.Server.Round.Stats.ScientistsDead);
+			cfgname = cfgname.Replace("$scientists_counter", "" + Counter(ScientistEscapes, ScientistStart));
+			cfgname = cfgname.Replace("$scp_alive", "" + ev.Server.Round.Stats.SCPAlive);
+			cfgname = cfgname.Replace("$scp_start", "" + SCPStart);
+			cfgname = cfgname.Replace("$scp_dead", "" + ev.Server.Round.Stats.SCPDead);
+			cfgname = cfgname.Replace("$scp_zombies", "" + ev.Server.Round.Stats.Zombies);
+			cfgname = cfgname.Replace("$scp_kills", "" + SCPKills);
+			cfgname = cfgname.Replace("$scp_counter", "" + Counter((ushort)ev.Server.Round.Stats.SCPAlive, SCPStart));
+			cfgname = cfgname.Replace("$ntf_alive", "" + ev.Server.Round.Stats.NTFAlive);
 			cfgname = cfgname.Replace("$ci_alive", "" + ev.Server.Round.Stats.CiAlive);
 			cfgname = cfgname.Replace("$warhead_detonated", WarheadDetonated ? "☢ WARHEAD DETONATED ☢" : "" );
-            cfgname = cfgname.Replace("$round_duration", "" + ev.Server.Round.Duration/60);
-            cfgname = cfgname.Replace("$round_number", "" + RoundNumber);
+			cfgname = cfgname.Replace("$round_duration", "" + ev.Server.Round.Duration/60);
+			cfgname = cfgname.Replace("$round_number", "" + RoundNumber);
 
 			foreach (System.Collections.Generic.KeyValuePair<string, Func<string>> entry in cmdtable)
-            {
-                cfgname = cfgname.Replace("$[" + entry.Key + "]", "" + entry.Value());
-            }
+			{
+				cfgname = cfgname.Replace("$[" + entry.Key + "]", "" + entry.Value());
+			}
 
-            ev.ServerName = cfgname;
-        }
+			ev.ServerName = cfgname;
+		}
 
-        public void OnRoundStart(RoundStartEvent ev)
-        {
-            RoundNumber++;
-            if (PluginManager.Manager.FindEnabledPlugins("LaterJoin").Count == 0)
-            {
-                foreach (Smod2.API.Player ply in ev.Server.GetPlayers())
-                {
-                    if (ply.TeamRole.Team == Smod2.API.Team.CLASSD)
-                    {
-                        ClassDStart++;
-                    }
-                    if (ply.TeamRole.Team == Smod2.API.Team.SCIENTISTS)
-                    {
-                        ScientistStart++;
-                    }
-                    if (ply.TeamRole.Team == Smod2.API.Team.SCP)
-                    {
-                        SCPStart++;
-                    }
-                }
-            }
-        }
+		public void OnRoundStart(RoundStartEvent ev)
+		{
+			RoundNumber++;
+			if (PluginManager.Manager.FindEnabledPlugins("LaterJoin").Count == 0)
+			{
+				foreach (Smod2.API.Player ply in ev.Server.GetPlayers())
+				{
+					if (ply.TeamRole.Team == Smod2.API.Team.CLASSD)
+					{
+						ClassDStart++;
+					}
+					if (ply.TeamRole.Team == Smod2.API.Team.SCIENTISTS)
+					{
+						ScientistStart++;
+					}
+					if (ply.TeamRole.Team == Smod2.API.Team.SCP)
+					{
+						SCPStart++;
+					}
+				}
+			}
+		}
 
-        public void OnRoundEnd(RoundEndEvent ev)
-        {
-            if (ev.Round.Duration >= 3)
-            {
-                reset();
-            }
-        }
+		public void OnRoundEnd(RoundEndEvent ev)
+		{
+			if (ev.Round.Duration >= 3)
+			{
+				reset();
+			}
+		}
 
-        public void OnPlayerDie(PlayerDeathEvent ev)
-        {
-            if (ev.Killer.TeamRole.Team == Smod2.API.Team.SCP)
-            {
-                SCPKills++;
-            }
-        }
+		public void OnPlayerDie(PlayerDeathEvent ev)
+		{
+			if (ev.Killer.TeamRole.Team == Smod2.API.Team.SCP)
+			{
+				SCPKills++;
+			}
+		}
 
-        public void OnCheckEscape(PlayerCheckEscapeEvent ev)
-        {
-            if (ev.Player.TeamRole.Role == Smod2.API.Role.CLASSD)
-            {
-                ClassDEscapes++;
-            }
-            if (ev.Player.TeamRole.Role == Smod2.API.Role.SCIENTIST)
-            {
-                ScientistEscapes++;
-            }
-        }
+		public void OnCheckEscape(PlayerCheckEscapeEvent ev)
+		{
+			if (ev.Player.TeamRole.Role == Smod2.API.Role.CLASSD)
+			{
+				ClassDEscapes++;
+			}
+			if (ev.Player.TeamRole.Role == Smod2.API.Role.SCIENTIST)
+			{
+				ScientistEscapes++;
+			}
+		}
 
-        public void OnSetRole(PlayerSetRoleEvent ev)
-        {
-            if (ev.Player.TeamRole.Team == Smod2.API.Team.NONE || blklst.Contains(ev.Player.SteamId)) { return; }
-            if (ev.TeamRole.Team == Smod2.API.Team.CLASSD)
-            {
-                ClassDStart++;
-            }
-            if (ev.TeamRole.Team == Smod2.API.Team.SCIENTISTS)
-            {
-                ScientistStart++;
-            }
-            if (ev.TeamRole.Team == Smod2.API.Team.SCP && ev.TeamRole.Role != Smod2.API.Role.SCP_049_2)
-            {
-                SCPStart++;
-            }
-            if (ev.TeamRole.Role != Smod2.API.Role.SPECTATOR)
-            {
-                blklst.Add(ev.Player.SteamId);
-            }
-        }
+		public void OnSetRole(PlayerSetRoleEvent ev)
+		{
+			if (ev.Player.TeamRole.Team == Smod2.API.Team.NONE || blklst.Contains(ev.Player.SteamId)) { return; }
+			if (ev.TeamRole.Team == Smod2.API.Team.CLASSD)
+			{
+				ClassDStart++;
+			}
+			if (ev.TeamRole.Team == Smod2.API.Team.SCIENTISTS)
+			{
+				ScientistStart++;
+			}
+			if (ev.TeamRole.Team == Smod2.API.Team.SCP && ev.TeamRole.Role != Smod2.API.Role.SCP_049_2)
+			{
+				SCPStart++;
+			}
+			if (ev.TeamRole.Role != Smod2.API.Role.SPECTATOR)
+			{
+				blklst.Add(ev.Player.SteamId);
+			}
+		}
 
-        public void OnDetonate()
-        {
-            WarheadDetonated = true;
-        }
-    }
+		public void OnDetonate()
+		{
+			WarheadDetonated = true;
+		}
+	}
 }
--- Main.cs
+++ Main.cs
@@ -6,22 +6,22 @@ using System;
 
 namespace ServerNameVars
 {
-    [PluginDetails(
-        author = "storm37000",
-        name = "ServerNameVars",
-        description = "Readds server name statistics plus some new ones",
-        id = "s37k.servernamevars",
-        version = "1.0.2",
-        SmodMajor = 3,
-        SmodMinor = 1,
-        SmodRevision = 0
-        )]
-    class Main : Plugin
-    {
-        public override void OnDisable()
-        {
-            this.Info(this.Details.name + " has been disabled.");
-        }
+	[PluginDetails(
+		author = "storm37000",
+		name = "ServerNameVars",
+		description = "Readds server name statistics plus some new ones",
+		id = "s37k.servernamevars",
+		version = "1.0.2",
+		SmodMajor = 3,
+		SmodMinor = 1,
+		SmodRevision = 0
+		)]
+	class Main : Plugin
+	{
+		public override void OnDisable()
+		{
+			this.Info(this.Details.name + " has been disabled.");
+		}
 		public override void OnEnable()
 		{
 			this.Info(this.Details.name + " has been enabled.");
@@ -60,34 +60,34 @@ namespace ServerNameVars
 
 		private EventHandler events;
 
-        public void addCustomVar(string varname, Func<string> callback, Plugin source)
-        {
-            events.addCustomVar(varname, callback, source);
-        }
+		public void addCustomVar(string varname, Func<string> callback, Plugin source)
+		{
+			events.addCustomVar(varname, callback, source);
+		}
 
-        public string testy()
-        {
-            return "Hello world";
-        }
+		public string testy()
+		{
+			return "Hello world";
+		}
 
-        public override void Register()
-        {
-            events = new EventHandler(this);
-            // Register Events
-            this.AddEventHandler(typeof(IEventHandlerSetServerName), events, Priority.Highest);
-            this.AddEventHandler(typeof(IEventHandlerRoundStart), events, Priority.Highest);
-            this.AddEventHandler(typeof(IEventHandlerRoundEnd), events, Priority.High);
-            this.AddEventHandler(typeof(IEventHandlerPlayerDie), events, Priority.Highest);
-            this.AddEventHandler(typeof(IEventHandlerCheckEscape), events, Priority.Highest);
-            if (PluginManager.Manager.FindEnabledPlugins("LaterJoin").Count > 0)
-            {
-                this.Info("Laterjoin integration enabled.");
-                this.AddEventHandler(typeof(IEventHandlerSetRole), events, Priority.Highest);
-            }
-            this.AddEventHandler(typeof(IEventHandlerWarheadDetonate), events, Priority.Highest);
+		public override void Register()
+		{
+			events = new EventHandler(this);
+			// Register Events
+			this.AddEventHandler(typeof(IEventHandlerSetServerName), events, Priority.Highest);
+			this.AddEventHandler(typeof(IEventHandlerRoundStart), events, Priority.Highest);
+			this.AddEventHandler(typeof(IEventHandlerRoundEnd), events, Priority.High);
+			this.AddEventHandler(typeof(IEventHandlerPlayerDie), events, Priority.Highest);
+			this.AddEventHandler(typeof(IEventHandlerCheckEscape), events, Priority.Highest);
+			if (PluginManager.Manager.FindEnabledPlugins("LaterJoin").Count > 0)
+			{
+				this.Info("Laterjoin integration enabled.");
+				this.AddEventHandler(typeof(IEventHandlerSetRole), events, Priority.Highest);
+			}
+			this.AddEventHandler(typeof(IEventHandlerWarheadDetonate), events, Priority.Highest);
 
-            Func<string> callback = testy;
-            this.addCustomVar("test", callback, this);
-        }
-    }
+			Func<string> callback = testy;
+			this.addCustomVar("test", callback, this);
+		}
+	}
 }
